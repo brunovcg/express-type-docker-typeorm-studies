6e55a2b2173614698daaa83af6d6d716da2b497e
Bruno Gouveia <78083006+brunovcg@users.noreply.github.com>
2022-07-30 02:07:20 -0300

    completed password resert

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,13 +7,14 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import uploadConfig from '@config/upload';
+import isAuthenticated from './middlewares/isAuthenticated';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', isAuthenticated, express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
 app.use(
