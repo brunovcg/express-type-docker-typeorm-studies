d20a73c2d12f74802e58ffbe9f41cf54265c8756
Bruno Gouveia <78083006+brunovcg@users.noreply.github.com>
2022-08-01 21:29:21 -0300

    class transformer

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -40,7 +40,7 @@ class SendForgotPasswordEmailService {
         variables: {
           name: user.name,
           // esse link é do front!
-          link: `http://localhost:3000/reset_password?token=${token}`,
+          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
         },
       },
     });
